3f09708c1b6a1002556168193ca9dd7e450ce194
mani varma <saimanikantavarma04@gmail.com>
2026-09-23 11:23:01 +0530

    refactor: replace Vite-specific env typings with generic Webpack env types

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+interface ImportMetaEnv {
+    readonly VITE_SUPABASE_URL: string
+    readonly VITE_SUPABASE_ANON_KEY: string
+    readonly VITE_RAZORPAY_KEY_ID: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
+
+interface Window {
+    Razorpay: any;
+}
